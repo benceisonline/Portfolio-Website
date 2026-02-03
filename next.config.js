b7b69eb7ab0953769f0a768f4eed662cd9039a14
Bence Szabo <benceszabo657@gmail.com>
2026-02-03 19:43:17 +0100

    dependency updates

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Allow the local network origin to access Next.js dev assets
+  allowedDevOrigins: [
+    'http://192.168.1.2'
+  ],
   images: {
     unoptimized: true,
   },
